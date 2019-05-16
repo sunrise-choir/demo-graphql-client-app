03aafb0172cfe7cff2c5fd82cd444c1c1b00cac1
Piet Geursen <pietgeursen@gmail.com>
2019-05-16 14:52:24 +1200

    Use useContext

--- components/scroller/scroller.js
+++ components/scroller/scroller.js
@@ -1,5 +1,5 @@
-import React, { Component, useState } from 'react'
-import { ApolloConsumer } from 'react-apollo'
+import React, { useContext, useState } from 'react'
+import { ApolloConsumer, ApolloProvider, ApolloContext } from 'react-apollo'
 import InfiniteScroll from 'react-infinite-scroller'
 import { CursorContext } from '../cursor/cursorProvider'
 
@@ -8,47 +8,40 @@ function Scroller (WrappedComponent, itemsKey) {
 
   return function (props) {
     const [items, setItems] = useState([])
+
+    const context = useContext(CursorContext)
+    const { client } = useContext(ApolloContext)
+
     const { query, variables: queryVars } = props
 
     return (
-      <CursorContext.Consumer>
-        {(context) => {
-          context = context || {}
-          return (<ApolloConsumer>
-            { client => {
-              return (
-                <InfiniteScroll
-                  pageStart={0}
-                  threshold={1000}
-                  loadMore={(page) => {
-                    console.log('loading more', endCursor, page)
-                    const variables = {
-                      after: page === 1 ? context.cursor : endCursor, // Use the global cursor for the first item. This scroller's first page is 1, even though we set pageStart to 0. Weird.
-                      first: 1, // Oddly, loading only one at a time means react is less laggy because it has less work to do for each setState call.
-                      ...queryVars
-                    }
+      <InfiniteScroll
+        pageStart={0}
+        threshold={1000}
+        loadMore={(page) => {
+          console.log('loading more', endCursor, page)
+          const variables = {
+            after: page === 1 ? context.cursor : endCursor, // Use the global cursor for the first item. This scroller's first page is 1, even though we set pageStart to 0. Weird.
+            first: 1, // Oddly, loading only one at a time means react is less laggy because it has less work to do for each setState call.
+            ...queryVars
+          }
 
-                    client.query({ query, variables })
-                      .then(({ data }) => {
-                        const { [itemsKey]: { edges, pageInfo: { endCursor: latestEndCursor } } } = data
+          client.query({ query, variables })
+            .then(({ data }) => {
+              const { [itemsKey]: { edges, pageInfo: { endCursor: latestEndCursor } } } = data
 
-                        const nodes = edges.map(edge => edge.node)
+              const nodes = edges.map(edge => edge.node)
 
-                        endCursor = latestEndCursor
-                        setItems((prevState) => prevState.concat(nodes))
-                      })
-                  }}
-                  hasMore
-                  useWindow
-                  loader={<div className='loader' key={0}>Loading ...</div>}
-                >
-                  <WrappedComponent items={items} />
-                </InfiniteScroll>
-              )
-            }}
-          </ApolloConsumer>)
+              endCursor = latestEndCursor
+              setItems((prevState) => prevState.concat(nodes))
+            })
         }}
-      </CursorContext.Consumer>
+        hasMore
+        useWindow
+        loader={<div className='loader' key={0}>Loading ...</div>}
+      >
+        <WrappedComponent items={items} />
+      </InfiniteScroll>
     )
   }
 }
